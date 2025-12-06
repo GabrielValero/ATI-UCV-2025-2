8cb349e5e3a7f484a68cbe21d782e759ba7e547c
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2025-12-05 23:55:23 -0400

    Modificaciones de optimizacion

--- js/index.js
+++ js/index.js
@@ -1,24 +1,32 @@
-function emptyResult(query){
+function emptyResult(query) {
     const gallery = document.querySelector('section ul');
-    
+
     gallery.innerHTML = `<p class="vacio">${config.vacio}"${query}"</p>`
 }
 function renderProfiles(profiles, language) {
     const gallery = document.querySelector('section ul');
-    gallery.innerHTML = `${profiles.map(profile => renderItem(profile, language)).join('')}`;
+    gallery.innerHTML = `${profiles.map(profile => {
+        console.log("Contexto en funcion flecha ->",this);
+        
+        return renderItem(profile, language)
+    }).join('')}`;
 }
 
 function renderItem(profile, lang) {
+    "use-strict";
+    console.log("Contexto en funcion normal ->",this);
     return `
-    <li>
+    <li class="perfilElement">
         <a href="perfil.html?ci=${profile.ci}&lang=${lang}">
             <img src="${profile.imagen}" alt="mi imágen">
-            <p>${profile.nombre}</p>
+            <p class="textPerfil">${profile.nombre}</p>
         </a>
     </li>`
 }
 
+console.log("este es el contexto Ambito global ->",this);
 window.onload = function () {
+    
     const language = new URLSearchParams(window.location.search).get("lang");
     if (!language) {
         let url = window.location.href;
@@ -33,7 +41,8 @@ window.onload = function () {
     const configlan = document.createElement('script');
     configlan.src = `/conf/config${language}.json`;
     configlan.onload = function () {
-
+        
+        
         const logo = document.querySelector('#nav1');
         logo.innerHTML = `<h1>${config.sitio[0]} <span>${config.sitio[1]}</span> ${config.sitio[2]}</h1>`;
         const footer = document.querySelector('footer p');
@@ -57,9 +66,9 @@ window.onload = function () {
         const query = searchBar.querySelector('.buscar').value.toLowerCase();
 
         const filteredProfiles = perfiles.filter(profile => profile.nombre.toLowerCase().includes(query));
-        if(filteredProfiles.length > 0 && !query.length === 0){
+        if (filteredProfiles.length > 0 && !query.length === 0) {
             renderProfiles(filteredProfiles);
-        }else{
+        } else {
             emptyResult(query)
         }
         e.preventDefault();
